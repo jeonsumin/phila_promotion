a45044500b1b60fad9d12bc1ed8a6a0bed0f9af1
jeonsumin <soomis7@naver.com>
2025-08-13 20:27:08 +0900

    commit

--- src/components/checkIn/ui/TermsDetail.tsx
+++ src/components/checkIn/ui/TermsDetail.tsx
@@ -1,4 +1,6 @@
 import {X} from "lucide-react";
+import {useSelector} from "react-redux";
+import {currentTranslation} from "features/changeLang";
 
 type PersonalInfoAlertProps = {
     terms: any;
@@ -6,16 +8,17 @@ type PersonalInfoAlertProps = {
 };
 
 export const TermsDetail = (props: PersonalInfoAlertProps) => {
+    const t = useSelector(currentTranslation);
     const {terms, onClose} = props;
 
     if (terms == null ) return null;
 
     return (
-        <div className='fixed top-0 left-0 w-full h-full z-[100] bg-white rounded-lg'>
+        <div className='fixed top-0 left-0 w-full h-full z-[100] bg-white rounded-lg  scrollbar'>
             <div
                 className={`modal_header flex items-center justify-center px-[16px] py-[16px]`}
             >
-                <h1 className="text-[var(--bk)] text-[20px] font-bold">{terms.title}</h1>
+                <h1 className="text-[var(--bk)] text-[20px] font-bold">{t(terms.title)}</h1>
                 <button
                     className="absolute right-[16px]"
                     onClick={onClose}
@@ -24,9 +27,9 @@ export const TermsDetail = (props: PersonalInfoAlertProps) => {
                     <X size={24} className="text-[var(--bk)]"/>
                 </button>
             </div>
-            <div className='terms_scroll'>
-                <p className='alert_content' style={{whiteSpace: 'pre-line'}}>
-                    {terms.desc}
+            <div className='p-4'>
+                <p className="bg-[#D9D9D9] p-4 rounded-md ">
+                    {t(terms.desc)}
                 </p>
             </div>
         </div>
